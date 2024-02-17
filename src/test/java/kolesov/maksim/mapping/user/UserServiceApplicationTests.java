fa8ae54ab2eec1635393bc128768423acf98f0d4
Maxim Kolesov <pro100maks222@gmail.com>
2024-02-17 17:59:00 +0300

    Initial commit

---
+++ src/test/java/kolesov/maksim/mapping/user/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kolesov.maksim.mapping.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
