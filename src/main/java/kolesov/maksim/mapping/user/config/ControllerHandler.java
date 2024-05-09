36968d781f1ac6055ba90da459ff6c655a39bcc5
Maxim Kolesov <pro100maks222@gmail.com>
2024-05-09 13:01:36 +0300

    add logs

--- src/main/java/kolesov/maksim/mapping/user/config/ControllerHandler.java
+++ src/main/java/kolesov/maksim/mapping/user/config/ControllerHandler.java
@@ -4,11 +4,13 @@ import kolesov.maksim.mapping.user.dto.ResponseDto;
 import kolesov.maksim.mapping.user.exception.ForbiddenException;
 import kolesov.maksim.mapping.user.exception.NotFoundException;
 import kolesov.maksim.mapping.user.exception.ServiceException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class ControllerHandler {
 
@@ -24,11 +26,13 @@ public class ControllerHandler {
 
     @ExceptionHandler(ServiceException.class)
     public ResponseEntity<ResponseDto<Void>> handleServiceException(ServiceException e) {
+        log.error("Bad request: ", e);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseDto<>(null, false, e.getMessage()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ResponseDto<Void>> handleException(Exception e) {
+        log.error("Internal server error: ", e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseDto<>(null, false, "Internal server error"));
     }
 
